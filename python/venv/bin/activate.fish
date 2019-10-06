bc79e3d59a816a8fd884df3c22d2987addc0476c
magnusref <magnusrc@gmx.com>
2019-10-06 22:53:13 +0200

    added .gitignore and recreated idea project

--- python/venv/bin/activate.fish
+++ python/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/magnusrc/Workspace/Python-CheatSheet/venv"
+set -gx VIRTUAL_ENV "/home/magnusrc/Workspace/CheatSheets/python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
