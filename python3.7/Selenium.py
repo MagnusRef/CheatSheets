967fae9b74dbfcb374e74d14f47097469292406a
magnusref <magnusrc@gmx.com>
2019-10-13 22:59:39 +0200

    simple selenium program

--- python3.7/Selenium.py
+++ python3.7/Selenium.py
@@ -1,2 +1,8 @@
 # install selenium via pip3
 from selenium import webdriver
+brw = webdriver.Firefox(executable_path='./geckodriver')
+brw.get("http://duckduckgo.com/")
+searchbar = brw.find_element_by_id("search_form_input_homepage")
+searchbar.send_keys("am I a bot?")
+searchgo = brw .find_element_by_id("search_button_homepage")
+searchgo.click()
